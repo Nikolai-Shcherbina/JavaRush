ab8be86f4ba58909c7068316af4aa85def5f1617
Nikolai Shcherbina <nikvikshch@gmail.com>
2021-08-30 22:34:40 +0300

    tasrs level 4

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -10,6 +10,13 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+        int c = Integer.parseInt(bufferedReader.readLine());
 
+        if (a + b <= c || a + c <= b || c + b <= a)
+             System.out.println("Треугольник не существует.");
+       else System.out.println("Треугольник существует.");
     }
 }
